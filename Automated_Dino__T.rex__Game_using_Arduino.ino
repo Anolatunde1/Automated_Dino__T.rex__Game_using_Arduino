a421913ed5684f2a3e9590a613022b234872c145
Olatunde <Olatunde>
2023-08-28 11:39:16 +0100

    new file:   Automated dino game/Automated Dino (T.rex) Game code_/Automated_Dino__T.rex__Game_using_Arduino.ino 	new file:   Automated dino game/Connection to arduino.png 	new file:   Automated_Dino__T.rex__Game_using_Arduino.ino

---
+++ Automated_Dino__T.rex__Game_using_Arduino.ino
@@ -0,0 +1,28 @@
+
+
+#include <Servo.h>
+#define threshold 250   // try 250 ,260 and 300 value here 
+#define unpress_angle 70 
+#define press_angle 36
+
+
+Servo myservo;  // create servo object to control a servo
+bool trig=true;
+
+void setup() {          
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+myservo.write(unpress_angle);  
+}
+
+void loop() {
+
+ myservo.write(unpress_angle);              // unpress the button
+ delay(1);
+ if(analogRead(A0)< threshold)
+ {
+  
+      myservo.write(press_angle);          // press the button
+    delay(100 );                       // waits 100ms for the servo to reach the position
+                      
+ }                     
+}
